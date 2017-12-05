2a047150e005191b47b92264fc7473062e7d7b30
uuhan <xuminhui189@gmail.com>
2017-12-05 21:44:22 +0800

    add server web support

--- server/Main.hs
+++ server/Main.hs
@@ -1,4 +1,30 @@
+{-# LANGUAGE QuasiQuotes     #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
 module Main (main) where
 
+import           Data.Text          (Text)
+import           Yesod.Core         as Y
+import           Yesod.Core.Handler as Y
+import           Yesod.WebSockets   as Y
+
+data Server = Server
+mkYesod "Server" [parseRoutes|
+/   RootR   GET
+|]
+
+instance Yesod Server
+
+serverApp :: WebSocketsT Handler ()
+serverApp = do
+    sendTextData ("todo" :: Text)
+
+getRootR :: Handler Html
+getRootR = do
+    webSockets serverApp
+    defaultLayout $
+      toWidget [hamlet|<div>server</div>|]
+
 main :: IO ()
-main = putStrLn "TODO"
+main = do
+    Y.warp 3301 Server
